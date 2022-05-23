ef44e397fae2351bb4421152f2df5532653b19b7
Mohamad Muradi <mohamadmuradi@Mohamads-MacBook-Air.local>
2022-05-23 11:00:44 +0300

    JS: Added the Global variables

---
+++ game.js
@@ -0,0 +1,9 @@
+window.onload=function(){
+
+    const number_1 = get_random();
+    const number_2 = get_random();
+
+    const img1 = document.getElementsByClassName("img1")[0];
+    const img2 = document.getElementsByClassName("img2")[0];
+
+}
